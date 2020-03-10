1c361d2d8c3a40ef669c33be03e71ce0f7634f68
Payson Wallach <paysonwallach@icloud.com>
2020-03-09 21:31:30 -0700

    Add GNOME Shell 3.36 support

--- hide-dash@xenatt.github.com/extension.js
+++ hide-dash@xenatt.github.com/extension.js
@@ -1,51 +1,25 @@
-const GLib = imports.gi.GLib;
-const Gio = imports.gi.Gio;
-const Lang = imports.lang;
 const Main = imports.ui.main;
 
+let observer, x, width;
 
-// extension functions
 function init() {
-	return new dashVisible();
+  x = Main.overview.viewSelector.actor.x;
+  width = Main.overview.viewSelector.actor.get_width();
 }
 
-const dashVisible = new Lang.Class({
-    Name: 'hideDash.dashVisible',
+function enable() {
+  observer = Main.overview.connect("showing", () => {
+    Main.overview._overview.dash.hide();
+    Main.overview.viewSelector.actor.set_x(0);
+    Main.overview.viewSelector.actor.set_width(0);
+    Main.overview.viewSelector.actor.queue_redraw();
+  });
+}
 
-	_init: function() {
-		this.observer = null;
-
-		// store the values we are going to override
-		this.old_x = Main.overview.viewSelector.actor.x;
-		this.old_width = Main.overview.viewSelector.actor.get_width();
-	},
-	
-	enable: function() {
-		// global.log("enable hide-dash");
-		this.observer = Main.overview.connect("showing", Lang.bind(this, this._hide));
-	},
-	
-	disable: function() {
-		// global.log("disable hide-dash");
-		Main.overview.disconnect(this.observer);
-		this._show();
-	},
-
-	_hide: function() {
-		// global.log("show dash");
-		Main.overview._dash.actor.hide();
-		Main.overview.viewSelector.actor.set_x(0);
-		Main.overview.viewSelector.actor.set_width(0);
-		Main.overview.viewSelector.actor.queue_redraw();
-	},
-
-	_show: function() {
-		// global.log("hide dash");
-		Main.overview._dash.actor.show();
-		Main.overview.viewSelector.actor.set_x(this.old_x);
-		Main.overview.viewSelector.actor.set_width(this.old_width);
-		Main.overview.viewSelector.actor.queue_redraw();
-	}
-});
-
-
+function disable() {
+  Main.overview.disconnect(observer);
+  Main.overview._overview.dash.show();
+  Main.overview.viewSelector.actor.set_x(x);
+  Main.overview.viewSelector.actor.set_width(width);
+  Main.overview.viewSelector.actor.queue_redraw();
+}
